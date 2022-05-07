f6aee869fd078d040827d96166f061104c87cbcf
Faires12 <faressolibie@gmail.com>
2022-05-07 13:00:21 -0300

    audio e correcoes

--- main.js
+++ main.js
@@ -1,23 +1,45 @@
-const socket = io.connect("https://chat-fares-movel.herokuapp.com")
+const socket = io.connect("https://chat-fares-movel.herokuapp.com/")
 
 const video = document.createElement("video")
 const canvas = document.createElement("canvas")
 canvas.width = 150
 canvas.height = 150
 const ctx = canvas.getContext("2d")
+let permitiuVideo = false
 
 navigator.mediaDevices.getUserMedia({video: {
     width: 150,
     height: 150
 }}).then(frame => {
+    permitiuVideo = true
     video.srcObject = frame
     video.play()
-})
+}).catch(() => permitiuVideo = false)
+
+var permitiuAudio = false
+var audio = ""
+const audioTag = document.createElement("audio")
+navigator.mediaDevices.getUserMedia({audio: true}).then( function(stream) {
+    function record_and_send() {
+        const recorder = new MediaRecorder(stream);
+        const chunks = [];
+        recorder.ondataavailable = e => chunks.push(e.data);
+        recorder.onstop = async (e) => {
+            var blob = new Blob(chunks,  { 'type' : 'audio/ogg; ' });
+            audio = await blobToBase64(blob)
+            socket.emit("getAudio", audio)
+        }
+        recorder.start();
+        setTimeout(()=> recorder.stop(), 350);    
+    }
+
+    setInterval(record_and_send, 350);
+}).catch(() => permitiuAudio = false);
 
 var playerInfos = {
     nome: '',
-    x: Math.floor(Math.random() * 1500),
-    y: Math.floor(Math.random() * 700),
+    x: Math.floor(Math.random() * 100),
+    y: Math.floor(Math.random() * 100),
     color: getRandomColor(),
     video: undefined
 }
@@ -44,37 +66,59 @@ document.addEventListener("keypress", e => {
     if(document.activeElement === document.getElementById("input") && e.key !== "Enter")
         return
 
-    if(e.key === "a")
-        playerInfos.x -= 20
-    if(e.key === "d")
-        playerInfos.x += 20
-    if(e.key === "w")
-        playerInfos.y -= 20
-    if(e.key === "s")
-        playerInfos.y += 20
+    if(e.key === "a" || e.key === "A")
+        playerInfos.x -= 2
+    if(e.key === "d" || e.key === "D")
+        playerInfos.x += 2
+    if(e.key === "w" || e.key === "W")
+        playerInfos.y -= 2
+    if(e.key === "s" || e.key === "S")
+        playerInfos.y += 2
     if(e.key === "Enter")
         sendMessage()
 
-    if(playerInfos.x < 0)
-        playerInfos.x = 0
-    if(playerInfos.x + 150 > 1500)
-        playerInfos.x = 1150
-    if(playerInfos.y < 0)
-        playerInfos.y = 0
-    if(playerInfos.y + 150 > 700)
-        playerInfos.y = 550
+    let porcPlayerX = (250/screen.width) * 50
+    let porcPlayerY = (250/screen.height) * 50
+    let porcInputY = (50/screen.height) * 100
+
+    if(playerInfos.x - porcPlayerX  < 0)
+        playerInfos.x = porcPlayerX
+    if(playerInfos.x + porcPlayerX > 100){
+        playerInfos.x = 100 - porcPlayerX
+    }    
+    if(playerInfos.y - porcPlayerY < 0)
+        playerInfos.y = porcPlayerY
+    if(playerInfos.y + porcPlayerY + porcInputY > 100)
+        playerInfos.y = 100 - porcPlayerY - porcInputY
 })
 
+function blobToBase64(blob) {
+    return new Promise((resolve, _) => {
+    const reader = new FileReader();
+    reader.onloadend = () => {resolve(reader.result);};
+    reader.readAsDataURL(blob);
+    });
+}
+
 setInterval(() => {
-    ctx.drawImage(video, 0, 0)
-    socket.emit("update", {x: playerInfos.x, y: playerInfos.y, video: canvas.toDataURL("image/jpeg")})
-}, 100)
+    let imgEnviada = ""
+    if(permitiuVideo){
+        ctx.drawImage(video, 0, 0)
+        imgEnviada = canvas.toDataURL("image/jpeg")
+    }
+    socket.emit("update", {x: playerInfos.x, y: playerInfos.y, video: imgEnviada})
+}, 50)
 
 socket.on("usersPos", users => {
     cleanScreen()
     for(let i in users){
         printPlayer(users[i], i)
     }
+})
+
+socket.on("audio", audio => {
+    const audioPlay = new Audio(audio)
+    audioPlay.play()
 })
 
 function cleanScreen(){
@@ -85,32 +129,33 @@ function cleanScreen(){
     }
 }
 
+
 function printPlayer(user, i){
     const div = document.createElement("div")
     div.className = "player"
-    div.style.left = `${user.x}px`
-    div.style.top = `${user.y}px`
-    div.style.backgroundColor = user.color
-
-    const img = document.createElement("img")
-    img.width = 150
-    img.height = 150
-    
-    img.src = user.video
-    div.appendChild(img)
+    div.style.left = `${user.x}%`
+    div.style.top = `${user.y}%`
 
     if(user.message){
         const message = document.createElement("section")
         message.innerHTML = user.message
-        message.style.position = "absolute"
-        message.style.top = "-40%"
-        message.style.padding = "10px"
-        message.style.borderRadius = "10px"
-        message.style.border = "1px solid black"
-        message.style.zIndex = "10000"
+        message.className = "message"
         div.appendChild(message)
     }
         
+    const img = document.createElement("img")
+    img.width = 250
+    img.height = 250
+    
+    img.src = user.video
+    div.appendChild(img)
+
+    const nome = document.createElement("section")
+    nome.innerHTML = user.nome
+    nome.style.marginTop = "10px"
+    nome.style.color = "white"
+    div.appendChild(nome)
+
     document.body.appendChild(div)
 }
 
